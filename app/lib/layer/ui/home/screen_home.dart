7ed5b4171ae8bdcb7971f9231c54c4a4045c04f1
Ihor Martyniuk <ihor.martyniuk@favbet.tech>
2026-01-03 18:35:46 +0200

    add localization

--- app/lib/layer/ui/home/screen_home.dart
+++ app/lib/layer/ui/home/screen_home.dart
@@ -1,3 +1,4 @@
+import 'package:app/l10n/app_localizations.dart';
 import 'package:flutter/material.dart';
 
 class ScreenHome extends StatelessWidget {
@@ -6,7 +7,10 @@ class ScreenHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Movies"), centerTitle: false),
+      appBar: AppBar(
+        title: Text(AppLocalizations.of(context)!.movies),
+        centerTitle: false,
+      ),
       body: Center(child: Text("HomeScreen")),
     );
   }
--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:app/core/const.dart';
+import 'package:app/l10n/app_localizations.dart';
 import 'package:app/layer/ui/root.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() {
   runApp(const _App());
@@ -9,10 +11,15 @@ void main() {
 class _App extends StatelessWidget {
   const _App();
 
+  final String _code_en = 'en';
+  final String _code_ua = 'uk';
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: Const.appName,
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
       theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
       home: const Root(),
     );
