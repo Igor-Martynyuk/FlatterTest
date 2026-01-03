48cf8dee93dd3fd79e42fad96ddc701f2d20a5a5
Ihor Martyniuk <ihor.martyniuk@favbet.tech>
2026-01-03 18:54:44 +0200

    cleanup

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -2,7 +2,6 @@ import 'package:app/core/const.dart';
 import 'package:app/l10n/app_localizations.dart';
 import 'package:app/layer/ui/root.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() {
   runApp(const _App());
