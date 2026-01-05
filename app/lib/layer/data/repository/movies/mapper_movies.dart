9ae5ddf0f27a04d85b72be0f8b123bef7466a96a
Ihor Martyniuk <ihor.martyniuk@favbet.tech>
2026-01-05 07:55:57 +0200

    minor: extend repository mapper

--- app/lib/layer/data/repository/movies/mapper_movies.dart
+++ app/lib/layer/data/repository/movies/mapper_movies.dart
@@ -1,7 +1,25 @@
 import 'package:app/layer/data/repository/movies/dto_movie.dart';
+import 'package:app/layer/data/repository/movies/dto_page.dart';
 import 'package:app/layer/data/source/tmdb/dto_tmdb_movie.dart';
+import 'package:app/layer/data/source/tmdb/dto_tmdb_page.dart';
+import 'package:flutter/cupertino.dart';
 
 class MapperMovies {
+  DtoPage mapPageDto(DtoTmdbPage tmbPage) {
+    final movies = List<DtoMovie>.empty(growable: true);
+
+    for (var item in tmbPage.results) {
+      try {
+        movies.add(mapMovieDto(item));
+      } catch (e) {
+        debugPrint("Movie processing failed: $item. Reason: $e");
+        continue;
+      }
+    }
+
+    return DtoPage(tmbPage.num, movies);
+  }
+
   DtoMovie mapMovieDto(DtoTmdbMovie tmdbDto) {
     return DtoMovie(
       tmdbDto.id!,
@@ -10,7 +28,7 @@ class MapperMovies {
       tmdbDto.popularity?.toDouble(),
       tmdbDto.overview,
       tmdbDto.releaseDate,
-      false
+      false,
     );
   }
 }
--- app/lib/layer/data/repository/movies/repository_movies.dart
+++ app/lib/layer/data/repository/movies/repository_movies.dart
@@ -2,7 +2,6 @@ import 'package:app/layer/data/repository/movies/dto_movie.dart';
 import 'package:app/layer/data/repository/movies/mapper_movies.dart';
 import 'package:app/layer/data/source/tmdb/source_tmdb_api.dart';
 import 'package:app/layer/domain/use/case/fetch/movies/port_fetch_movies.dart';
-import 'package:flutter/cupertino.dart';
 
 class RepositoryMovies implements PortFetchMovies {
   final SourceTmdbAPI _remoteSource;
@@ -13,17 +12,6 @@ class RepositoryMovies implements PortFetchMovies {
   @override
   Future<List<DtoMovie>> loadTopRated(int page) async {
     final remoteData = await _remoteSource.loadTopRated(page);
-    final result = List<DtoMovie>.empty(growable: true);
-
-    for (var item in remoteData.results) {
-      try {
-        result.add(_mapper.mapMovieDto(item));
-      } catch (e) {
-        debugPrint("Movie processing failed: $item. Reason: $e");
-        continue;
-      }
-    }
-
-    return result;
+    return _mapper.mapPageDto(remoteData).movies;
   }
 }
